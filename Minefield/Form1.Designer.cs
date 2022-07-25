e717b7a593be7a93366f523e6b3ea505b9bd3d6f
Mutlucan Gokcukur <211307006@uzem.education>
2022-07-25 12:26:52 +0300

    upload

---
+++ Minefield/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Minefield
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.lblMayınSayı = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(818, 254);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(856, 39);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(133, 68);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSkor.Location = new System.Drawing.Point(852, 119);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(47, 20);
+            this.lblSkor.TabIndex = 2;
+            this.lblSkor.Text = "Skor";
+            // 
+            // lblMayınSayı
+            // 
+            this.lblMayınSayı.AutoSize = true;
+            this.lblMayınSayı.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMayınSayı.Location = new System.Drawing.Point(852, 155);
+            this.lblMayınSayı.Name = "lblMayınSayı";
+            this.lblMayınSayı.Size = new System.Drawing.Size(115, 20);
+            this.lblMayınSayı.TabIndex = 3;
+            this.lblMayınSayı.Text = "Mayın Sayısı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1022, 279);
+            this.Controls.Add(this.lblMayınSayı);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minefield";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label lblMayınSayı;
+    }
+}
+
---
+++ Minefield/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minefield
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Tanımlamalar
+        int mayın1 = 0;
+        int mayın2 = 0;
+        int mayın3 = 0;
+        int mayın4 = 0;
+        int mayın5 = 0;
+        int skor = 0;
+        int mayınsayısı = 5;
+        Random rnd = new Random();
+        #endregion
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            #region Mayınları Random Atama
+            mayın1 = rnd.Next(0, 71);
+            mayın2 = rnd.Next(0, 71);
+            mayın3 = rnd.Next(0, 71);
+            mayın4 = rnd.Next(0, 71);
+            mayın5 = rnd.Next(0, 71);
+            #endregion
+
+            #region Buton Oluşturma
+            for (int i = 1; i < 71; i++)
+            {
+                Button buton = new Button();
+                buton.Name = "btn" + i.ToString();
+                buton.Size = new System.Drawing.Size(35, 35);
+                buton.Text = "*";
+                if (mayın1==i || mayın2==i ||mayın3==i || mayın4==i || mayın5==i)
+                {
+                    buton.Tag = true;   
+                }
+                else
+                {
+                    buton.Tag = false;
+                }
+                buton.Click += buton_Click;
+                buton.UseVisualStyleBackColor = true;
+                flowLayoutPanel1.Controls.Add(buton);
+            }
+            #endregion
+        }
+
+        private void buton_Click(object sender, EventArgs e)
+        {
+
+            Button basilanButon = ((Button)sender);
+            bool BulunanMayın = (bool)basilanButon.Tag;
+            #region Mayın Bulunduğu Zaman
+            if (BulunanMayın)
+            {
+                basilanButon.BackColor = Color.Red;
+                mayınsayısı--;
+                lblMayınSayı.Text = "Kalan Mayın: " + mayınsayısı;
+                basilanButon.Enabled = false;
+                #region Mayın Bittiği Zaman
+                if (mayınsayısı==0)
+                {
+                    DialogResult result= MessageBox.Show("Bütün Mayınları Buldunuz" +
+                    "\nTekrar Oynamak İster Misiniz?", "Bilgilendirme"
+                    ,MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                    if (result==DialogResult.Yes)
+                    {
+                        Application.Restart();
+                    }
+                    else if(result==DialogResult.No)
+                    {
+                        Application.ExitThread();
+                    }
+                    
+                }
+                #endregion
+            }
+            else
+            {
+                basilanButon.BackColor = Color.Green;
+                skor += 2;
+                lblSkor.Text = "Skorunuz: " + skor;
+                basilanButon.Enabled = false;
+            }
+            #endregion
+        }
+    }
+}
